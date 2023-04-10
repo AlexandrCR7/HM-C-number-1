a55c39480e9b34f23be072aedbde27c0080f351f
Alexandr <k9108@mail.ru>
2023-04-10 18:01:51 +0300

    Разношу программы по разным папкам, это задача №2

--- Задача 2/Program.cs
+++ Задача 2/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите первое число: ");
+int i = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int x = int.Parse(Console.ReadLine());
+if(i > x){
+    System.Console.WriteLine("Максимальное число = " + i);
+}
+else{
+    System.Console.WriteLine("Максимальное число = " + x);
+}
