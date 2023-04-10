cb39fc96fa53b30feae691c1c60b4286a0a9f9ba
Alexandr <k9108@mail.ru>
2023-04-10 18:12:55 +0300

    Add

---
+++ Задача 6/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите первое число: ");
+int i = int.Parse(Console.ReadLine());
+if(i % 2 == 0){
+    System.Console.WriteLine("Это четное число");
+}
+else{
+    System.Console.WriteLine("Это нечетное число");
+}
