59e3cbafc020c70144f8dee9de4f3a2be5e70b4e
Alexandr <k9108@mail.ru>
2023-04-10 18:07:15 +0300

    Ща добавлю 2ю задачу

--- Задача 4/Program.cs
+++ Задача 4/Program.cs
@@ -1,2 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿System.Console.WriteLine("Введите первое число: ");
+int first = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите второе число: ");
+int second = int.Parse(Console.ReadLine());
+System.Console.WriteLine("Введите третье число: ");
+int third = int.Parse(Console.ReadLine());
+
+if(first > second && first > third){
+        System.Console.WriteLine("Максимальное число " + first);
+    }
+
+else if(second > third){
+    System.Console.WriteLine("Максимальное число " + second);
+    }
+
+else{
+System.Console.WriteLine("Максимальное число " + third);
+}
