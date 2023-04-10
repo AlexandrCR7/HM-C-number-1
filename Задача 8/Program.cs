2c3367ba0b94eb39fcfe02d3619ce90a97b18312
Alexandr <k9108@mail.ru>
2023-04-10 18:15:21 +0300

    Add

---
+++ Задача 8/Program.cs
@@ -0,0 +1,8 @@
+﻿System.Console.WriteLine("Введите число: ");
+int N = int.Parse(Console.ReadLine());
+int i = 1;
+while(i <= N){
+     if(i % 2 == 0)
+     System.Console.Write(i + ", ");
+     i++;
+}
